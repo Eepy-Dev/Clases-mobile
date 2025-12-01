b7f34dd8216d06c69a0eb3b365648f2406233f08
Vi8let <pazulinaa@gmail.com>
2025-12-01 02:11:55 -0300

    feat: Add Room database setup with `ProductEntity` and `DeletedProductEntity` and their DAOs.

--- app/src/main/java/com/example/appmovil/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/appmovil/data/local/AppDatabase.kt
@@ -4,6 +4,8 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.appmovil.data.local.dao.DeletedProductDao
 import com.example.appmovil.data.local.entity.DeletedProductEntity
+import com.example.appmovil.data.local.dao.ProductDao
+import com.example.appmovil.data.local.entity.ProductEntity
 
 @Database(entities = [ProductEntity::class, DeletedProductEntity::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
